5d0f91442c052f72ed2371b5b303d5e13268dfeb
david gurcinoski <david_ddmon@hotmail.com>
2022-03-10 12:00:06 -0800

    Update

--- Class-2/Class-2/CSharpG2-Class2-Operators/Program.cs
+++ Class-2/Class-2/CSharpG2-Class2-Operators/Program.cs
@@ -46,6 +46,68 @@ namespace CSharpG2_Class2_Operators
 
             //Exe 3,4 HomeWork
 
+            Console.WriteLine("Enter number of credits:");
+            string numberOfCreditsString = Console.ReadLine();
+            Console.WriteLine("Enter cost of 1 SMS:");
+            string costOfSMSString = Console.ReadLine();
+
+            int numberOfCreditsInt = int.Parse(numberOfCreditsString);
+            int costOfSMSInt = int.Parse(costOfSMSString);
+
+            int numberOfSMSSends = numberOfCreditsInt / costOfSMSInt;
+
+            Console.WriteLine($"You can send {numberOfSMSSends} SMS with this amount of credits {numberOfCreditsString}");
+
+            Console.WriteLine("Enter Two Numbers:");
+            string inputNumOne = Console.ReadLine();
+            string inputNumTwo = Console.ReadLine();
+
+            int.TryParse(inputNumOne, out int inputNumOneInt);
+            int.TryParse(inputNumTwo, out int inputNumTwoInt);
+            string biggerNumber;
+            if (inputNumOneInt != inputNumTwoInt)
+            {
+                biggerNumber = (inputNumOneInt > inputNumTwoInt) ? inputNumOne : inputNumTwo;
+            }
+            else
+            {
+                biggerNumber = "The nums are The same";
+            }
+
+            Console.WriteLine($"The number {biggerNumber} is bigger");
+            Console.WriteLine("The input one is ");
+            Console.Write((inputNumOneInt % 2) == 0 ? "Even" : "Odd");
+            Console.WriteLine("The input two is ");
+            Console.Write((inputNumTwoInt % 2) == 0 ? "Even" : "Odd");
+
+
+            //Exe 6 Home
+
+            Console.WriteLine("Enter a number between 1 to 3");
+            string input = Console.ReadLine();
+            if (
+            int.TryParse(input, out int inputToInt) && (inputToInt <= 1) && (inputToInt >= 3)
+               )
+            {
+                switch (inputToInt)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("You got a NEW HOUSE");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("You got a NEW CAR");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("You got a NEW SOMETHING ELSE");
+                            break;
+                        }
+                }
+            }
         }
     }
 }
