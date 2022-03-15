90094710f3639b93e1d36e2f924e735162e6f7bf
david gurcinoski <david_ddmon@hotmail.com>
2022-03-15 11:49:42 -0700

    Fix

---
+++ Class-3/Class-3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Class_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter a number:");
+
+            //string inputStringOne = Console.ReadLine();
+            //int.TryParse(inputStringOne, out int inputIntOne);
+
+            //for(int counter = 2; counter <= inputIntOne; counter += 2)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //string inputStringTwo = Console.ReadLine();
+            //int.TryParse(inputStringTwo, out int inputIntTwo);
+
+            //for (int counter = 1; counter <= inputIntTwo; counter += 2)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //Console.WriteLine("Enter a number");
+
+            //string inputStringOne = Console.ReadLine();
+
+            //bool isInputNumber = int.TryParse(inputStringOne, out int inputIntOne);
+
+            //if(isInputNumber)
+            //{
+            //    for (int counter = 1; counter <= inputIntOne; counter++)
+            //    {
+            //        if ((counter % 3 == 0) || (counter % 7 == 0))
+            //        {
+            //            continue;
+            //        }
+            //        if (counter >= 100)
+            //        {
+            //            Console.WriteLine("The limet is reached!");
+            //            break;
+            //        }
+            //        Console.WriteLine(counter);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Enter only numbers!");
+            //}
+
+
+        }
+    }
+}
