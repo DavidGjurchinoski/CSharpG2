03f616e3e628e2e33388c5a7a15f34c569444800
DavidGjurchinoski <davidddmondavid@gmail.com>
2022-03-16 21:46:06 +0100

    Update

---
+++ Homeworks/Homework2/Task1/SumOfEven.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework2
+{
+    class SumOfEven
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfInts = new int[6];
+            bool hasNoError = true;
+            int sumOfEvenNums = 0;
+
+            for (int counter = 0; counter < arrayOfInts.Length; counter++)
+            {
+                Console.WriteLine("Enter a number:");
+
+                hasNoError = int.TryParse(Console.ReadLine(), out int numInput);
+
+                if (hasNoError)
+                {
+                    arrayOfInts[counter] = numInput;
+                }
+                else
+                {
+                    Console.WriteLine("Enter only numbers!");
+                    counter--;
+                }
+            }
+
+            foreach(int number in arrayOfInts)
+            {
+                if(number % 2 == 0)
+                {
+                    sumOfEvenNums += number;
+                }
+            }
+
+            Console.WriteLine($"The result is: {sumOfEvenNums}");
+        }
+    }
+}
