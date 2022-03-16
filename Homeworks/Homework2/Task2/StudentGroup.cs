35db0314a6b641c029a53e5ca22fa08ae449aeac
DavidGjurchinoski <davidddmondavid@gmail.com>
2022-03-16 22:20:47 +0100

    Fix

--- Homeworks/Homework2/Task2/StudentGroup.cs
+++ Homeworks/Homework2/Task2/StudentGroup.cs
@@ -6,7 +6,32 @@ namespace StudentGroup
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] studentsGOne = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsGTwo = { "Zoki", "Mile", "Brus", "Petko", "Vele" };
+
+            Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("The Students in G1 are:");
+                    foreach (string name in studentsGOne)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("The Students in G2 are:");
+                    foreach (string name in studentsGTwo)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("You did not enter a valid group (1 or 2)!");
+                    break;
+            }
+
         }
     }
 }
